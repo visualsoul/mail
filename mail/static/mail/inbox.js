4da8fd388c1db1bd1eabc0910aafa937fb55b5c9
visualsoul <zbynek.vrzalik@gmail.com>
2020-09-26 23:52:12 +0100

    updated send email functionality 2 - needs fixing

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -56,43 +56,8 @@ function view_email(email_id) {
 }
 
 
-function compose_email() {
-
-  // Show compose view and hide other views
-  document.querySelector('#email-view').style.display = 'none';
-  document.querySelector('#emails-view').style.display = 'none';
-  document.querySelector('#compose-view').style.display = 'block';
-
-  // Clear out composition fields
-  document.querySelector('#compose-recipients').value = '';
-  document.querySelector('#compose-subject').value = '';
-  document.querySelector('#compose-body').value = '';
-
-  document.querySelector('#submit_mail').addEventListener('click', () => {
-      fetch('/emails', {
-      method: 'POST',
-      body: JSON.stringify({
-      recipients: document.querySelector('#compose-recipients').value,
-      subject: document.querySelector('#compose-subject').value,
-      body: document.querySelector('#compose-body').value
-        })
-        })
-        .then(response => response.json())
-        .then(result => {
-        // Print result
-        console.log(result);
-    });
-
-
-  });
-
-
-
-
-}
-
 function load_mailbox(mailbox) {
-  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
@@ -102,7 +67,7 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   //  Get inbox data
-  fetch('/emails/inbox')
+  fetch(`/emails/${mailbox}`)
     .then(response => response.json())
     .then(emails => {
     // Print emails
@@ -156,3 +121,41 @@ function load_mailbox(mailbox) {
 
 
 }
+
+
+function compose_email() {
+  // Show compose view and hide other views
+  document.querySelector('#email-view').style.display = 'none';
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = '';
+  document.querySelector('#compose-subject').value = '';
+  document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#submit_mail').addEventListener('click', () => {
+      fetch('/emails', {
+      method: 'POST',
+      body: JSON.stringify({
+      recipients: document.querySelector('#compose-recipients').value,
+      subject: document.querySelector('#compose-subject').value,
+      body: document.querySelector('#compose-body').value
+        })
+        })
+        .then(response => response.json())
+        .then(result => {
+        // Print result
+        console.log(result);
+        load_mailbox('sent');
+    })
+
+
+
+  })
+
+
+
+
+}
+
